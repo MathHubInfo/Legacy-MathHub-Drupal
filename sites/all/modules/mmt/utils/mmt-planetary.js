a43502f0c52270f8463c84a916962354d7612e3e
m-iancu <mihnea.iancu88@gmail.com>
2014-10-15 15:52:11 +0200

    fixed uri-processing bug in js navigation

--- sites/all/modules/mmt/utils/mmt-planetary.js
+++ sites/all/modules/mmt/utils/mmt-planetary.js
@@ -1,33 +1,30 @@
-
 var planetary = {
+
   relNavigate: function(uri) {
-  	var rawEncoded = encodeURIComponent(uri);
-	var matches = uri.match(/^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/);
-	var fragment = ""; //default
-	if (matches[7] != undefined) {
-		fragment = "#" + matches[7].substring(1); //removing beginning '?'
+  	var loc = window.location.pathname;
+  	var locComps = loc.split("/");
+	var uriMatches = uri.match(/^(http[s]?:\/\/)?([^:\/\s]+)(.*)$/);
+	var uriComps = uriMatches[3].split("/");
+	var last = uriComps[uriComps.length - 1];
+	if (locComps[locComps.length - 1] == "") {
+		locComps[locComps.length - 1] = last; // uri ends in slash => replace
+	} else {
+		locComps.push(last); //add uri comp
 	}
-	var path = matches[4] + "source/" + matches[6] + fragment;
-	var pathname = window.location.pathname;
-	if (pathname.charAt(pathname.length - 1) !== '/') {
-		pathname = pathname + "/";
-	} 
-	var path = pathname + matches[6];
+	var path = locComps.join("/");
 	window.location = path;
   },
 
-  navigate: function(uri) {
-	window.location = planetary.encode(uri);
+  URIToURL: function(uri) {
+	var matches = uri.match(/^(http[s]?:\/\/)?([^:\/\s]+)(.*)$/);
+	var comps = matches[3].split("/");
+	comps.splice(3,0,"source");
+	var path = comps.join("/");
+	return path;
   },
-  
-  encode : function(uri) {
-	var rawEncoded = encodeURIComponent(uri);
-	var matches = uri.match(/^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/);
-	var fragment = ""; //default
-	if (matches[7] != undefined) {
-		fragment = "#" + matches[7].substring(1); //removing beginning '?'
-	}
-	var path = matches[4] + "source/" + matches[6] + fragment;
-		return path;
-    },
+
+  navigate: function(uri) {
+  	var path = URIToURL(uri);
+  	window.location = path;
+  },  
 };
