06764e6a3571710ed914ae68e5f7dc32145a6a70
m-iancu <mihnea.iancu88@gmail.com>
2014-11-25 15:17:50 +0100

    updated admin interface after compiler refactoring

--- sites/all/modules/oaff/oaff.admin.php
+++ sites/all/modules/oaff/oaff.admin.php
@@ -25,16 +25,10 @@ function oaff_admin_menu(& $items) {
   	'access callback' => 'oaff_admin_access',
     'type' => MENU_CALLBACK,
   );
-  $items['mh/lmh-gen-omdoc'] = array(
-  	'title' => "Lmh Generate OMDoc",
-  	'page callback' => 'oaff_admin_lmh_gen_omdoc',
-  	'access callback' => 'oaff_admin_access',
-    'type' => MENU_CALLBACK,
-  );
-  $items['mh/mmt-rebuild'] = array(
-  	'title' => "Rebuild MMT",
-  	'page callback' => 'oaff_admin_mmt_rebuild',
-  	'access callback' => 'oaff_admin_access',
+  $items['mh/generate-glossary'] = array(
+    'title' => "Regenerate Glossary",
+    'page callback' => 'oaff_admin_generate_glossary',
+    'access callback' => 'oaff_admin_access',
     'type' => MENU_CALLBACK,
   );
   $items['mh/administrate_mathhub'] = array(
@@ -42,7 +36,7 @@ function oaff_admin_menu(& $items) {
   	'page callback' => 'oaff_admin_administrate',
   	'access callback' => 'oaff_admin_access',
   	'menu_name' => 'navigation',
-  );
+  );  
   return $items;
 }
 
@@ -154,18 +148,16 @@ function oaff_admin_crawl_nodes() {
 
 function oaff_admin_administrate() {
   $out  = '<h4> This page provides some admin-level functionality for MathHub.info </h4>';
-  $out .= '<ul> <li> Get The latest version of the source documents ';
+  $out .= '<ul> <li> Get the latest version of the source documents ';
   $out .= '<button onclick="window.location = \'/mh/lmh-update\'" class="btn btn-primary btn-xs"> Lmh Update </button> </li>';
-  $out .= '<li> Regenerate OMDoc (runs in background) ';
-  $out .= '<button onclick="window.location = \'/mh/lmh-gen-omdoc\'" class="btn btn-warning btn-xs"> Lmh Generate OMDoc </button> </li>';
   $out .= '<li> Update Libraries (sTeX, MMT) ';
   $out .= '<button onclick="window.location = \'/mh/libs-update\'" class="btn btn-primary btn-xs"> Update Libs </button> </li>';
   $out .= '<li> Touch Source Files (useful in case of compiler update to mark them as modified for crawler) ';
   $out .= '<button onclick="window.location = \'/mh/touch-files\'" class="btn btn-primary btn-xs"> Touch Files </button> </li>';
-  $out .= '<li> Rebuild MMT archives ';
-  $out .= '<button onclick="window.location = \'/mh/mmt-rebuild\'" class="btn btn-primary btn-xs"> Rebuild MMT Archives </button> </li>';  
   $out .= '<li> Crawl Loaded Nodes (normally handled by cron, run manually if needed) ';
-  $out .= '<button onclick="window.location = \'/mh/crawl-nodes\'" class="btn btn-primary btn-xs"> Crawl Nodes </button> </li> </ul>';
+  $out .= '<button onclick="window.location = \'/mh/crawl-nodes\'" class="btn btn-primary btn-xs"> Crawl Nodes </button> </li> ';
+  $out .= '<li> Regenerate Glossary ';
+  $out .= '<button onclick="window.location = \'/mh/generate-glossary\'" class="btn btn-primary btn-xs"> Regenerate </button> </li> </ul>';
   //$out .= ' <button class="btn btn-primary " onclick="window.location = \'/mh/crawl-nodes\'"> Continue </button> </div> ';
   return $out;
 }
@@ -177,13 +169,6 @@ function oaff_admin_lmh_update() {
   return '';
 }
 
-function oaff_admin_lmh_gen_omdoc() {
-	$lmh_status = shell_exec('lmh gen --omdoc /var/data/localmh/MathHub/ >/dev/null 2 >/dev/null &');
-	oaff_log("OAFF.ADMIN", "`lmh gen --omdoc /var/data/localmh/MathHub/` called in the background");
-	drupal_set_message('Success');
-	return '';
-}
-
 function oaff_admin_libs_update() {
 	$git_log = shell_exec('cd /var/data/localmh/ext/sTeX/ && git pull 2>&1 && cd /var/data/localmh/ext/MMT/ && svn up 2>&1');
 	oaff_log("OAFF.ADMIN", "`git pull` returned: <pre>$git_log</pre>");
@@ -191,9 +176,11 @@ function oaff_admin_libs_update() {
 	return '';
 }
 
-function oaff_admin_mmt_rebuild() {
-	$mmt_log = shell_exec('/var/data/localmh/ext/MMT/rebuild-mathhub.sh 2>&1');
-	oaff_log("OAFF.ADMIN", "`rebuild-mathhub.sh` returned: <pre>$mmt_log</pre>");
-	drupal_set_message('Success');
-	return '';
+function oaff_admin_generate_glossary() {
+  $mmt_config = variable_get("mmt_config");
+  $mmturl = $mmt_config['mmturl'];
+  $out = file_get_contents($mmturl . '/:planetary/generateGlossary');
+  oaff_log("OAFF.ADMIN", "Regenerated Glossary");
+  drupal_set_message($out);
+  return '';
 }
