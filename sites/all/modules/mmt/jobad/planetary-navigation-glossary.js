32ec5cc3f280ea2b24874fd43f37ebfaf79ac21f
m-iancu <mihnea.iancu88@gmail.com>
2015-04-09 20:04:01 +0200

    glossary js update

--- sites/all/modules/mmt/jobad/planetary-navigation-glossary.js
+++ sites/all/modules/mmt/jobad/planetary-navigation-glossary.js
@@ -23,13 +23,25 @@ var planetaryNavigationGlossary = {
 
     contextMenuEntries: function(target, JOBADInstance) {
     	var res = {};
+
 		if (target.hasAttribute('jobad:href')) {			
 			var mr = $(target).closest('mrow');
 			var select = (mr.length === 0) ? target : mr[0];
 			mmt.setSelected(select);
 			var uri = target.attr('jobad:href');
+			var matches = uri.match(/^(http[s]?:\/\/)?([^:\/\s]+)(.*)$/);
+			var comps = matches[3].split("/");
+			var group = comps[1]; //0 is empty string since matches[3] starts with '/'
+			var arch = comps[2];
+			var relPath = comps.slice(3).join("/");
+			var frags = relPath.split(".");
+    	    frags[frags.length - 1] = "tex"; //settings extension
+    	    var tex_path = frags.join(".");
+		    var blob_url = 'https://gl.mathhub.info/' + group  + "/" + arch + "/blob/master/source/" + tex_path;
 			var me = this;
+			res['Go To Source'] =  function() {window.open(blob_url, '_blank');},
 			res['Go To Declaration'] = function() {planetary.navigate(uri);};
+
 		}
 		return res;
 	},
