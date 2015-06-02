da6c546719839d64fde9f6b9e143f1ee69c8a6da
aoripov <akbarzoripov@gmail.com>
2015-06-02 15:42:06 +0200

    Add more functionality to oaff_generate_tabs Use tabs for error log in node

--- sites/all/modules/oaff/oaff.html_utils.php
+++ sites/all/modules/oaff/oaff.html_utils.php
@@ -5,14 +5,15 @@
 // data -- array of dictionaries with 2 fields:
 //		'title'
 //		'content'
-function oaff_generate_tabs($data) {
+// default -- default active tab 
+function oaff_generate_tabs($data, $default) {
 	$out = "<div role=\"tabpanel\">";
 	$out .= "	<ul class=\"nav nav-tabs\" role=\"tablist\">";
 	// generate tab titles
 	for ($i = 0; $i < sizeof($data); $i++) {
 		$id = str_replace(" ", "_", strtolower($data[$i]['title']));
 		// default tab
-		if ($i == 0) {
+		if ($i == $default) {
 			$out .= "<li role=\"presentation\" class=\"active\"><a href=\"#$id\" aria-controls=\"$id\" role=\"tab\" data-toggle=\"tab\">".$data[$i]['title']."</a></li>";
 		} else {
 			$out .= "<li role=\"presentation\"><a href=\"#$id\" aria-controls=\"$id\" role=\"tab\" data-toggle=\"tab\">".$data[$i]['title']."</a></li>";
@@ -23,7 +24,7 @@ function oaff_generate_tabs($data) {
 	$out .= "	<div class=\"tab-content\">";
 	for ($i = 0; $i < sizeof($data); $i++) {
 		$id = str_replace(" ", "_", strtolower($data[$i]['title']));
-		if ($i == 0) {
+		if ($i == $default) {
 			$out .= "<div role=\"tabpanel\" class=\"tab-pane active\" id=\"$id\">".$data[$i]['content']."</div>";
 		} else {
 			$out .= "<div role=\"tabpanel\" class=\"tab-pane\" id=\"$id\">".$data[$i]['content']."</div>";
