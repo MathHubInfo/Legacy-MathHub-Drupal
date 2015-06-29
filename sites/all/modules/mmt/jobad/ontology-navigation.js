bf0a1d3928c6b33b66cda391094e10cc1198e4df
aoripov <akbarzoripov@gmail.com>
2015-06-29 17:55:42 +0200

    Categorize items in Uses/Used in menu

--- sites/all/modules/mmt/jobad/ontology-navigation.js
+++ sites/all/modules/mmt/jobad/ontology-navigation.js
@@ -24,20 +24,95 @@ var ontologyNavigation = {
 
     getRelated: function(uri, relation) {
     	var query = qmt.related(qmt.literalPath(uri), relation);
-    	var related_uris = {};
+    	var related_uris = [];
     	var me = this;
     	qmt.exec(query, 
     			 function(data) { 
     				$(data).find("uri").each(function (i, val) {
     					var path = $(val).attr('path');
-    					related_uris[planetary.URIToURL(path)] = function() {planetary.navigate(path);};
+    					related_uris.push([planetary.URIToURL(path).split("/").slice(1), function() {planetary.navigate(path);}]);
     				});
     			 },
     			 false);
-    	return related_uris;
+        // disable if no data
+        if (related_uris.length == 0)
+    	   return false;
+        else
+           return me.buildSubMenus(related_uris);
     },
 
-	
+    buildSubMenus: function(uris, prefix="") {
+        if (uris.length  == 0) 
+            return {};
+        if (prefix != "") {
+            prefix += "/";
+        }
+        var temp_uris = {};
+        var sorted = uris.sort(
+            function(a, b){
+                var a_ = a[0][0];
+                var b_ = b[0][0];
+                return a_ > b_;
+            });
+        var result = {};
+        for (var i in sorted) {
+            if (sorted[i][0].length == 1) {
+                result[prefix + sorted[i][0][0]] = sorted[i][1];
+
+            } else { 
+                
+                if (typeof temp_uris[sorted[i][0][0]] == 'undefined') {
+                    temp_uris[sorted[i][0][0]] = [[sorted[i][0].slice(1),sorted[i][1]]];
+                } else {
+                    temp_uris[sorted[i][0][0]].push([sorted[i][0].slice(1),sorted[i][1]]);
+                }
+            }
+        }
+
+        result = this.buildAlphaSubMenu(result);
+
+        if (this.objectLength(temp_uris) == 1) {
+            for (var key in temp_uris) {
+                jQuery.extend(result, this.buildSubMenus(temp_uris[key],prefix + key));
+            }   
+        } else {
+            for (var key in temp_uris) {
+                result[prefix + key] = this.buildSubMenus(temp_uris[key]);
+            }
+        }
+
+        return result;
+    },
+
+    objectLength: function(obj) {
+        var length = 0;
+        for (var key in obj) {
+            length++;
+        }
+        return length;
+    },
+
+    buildAlphaSubMenu: function(uris, depth = 0) {
+        if (this.objectLength(uris) <= 10)
+            return uris;
+        result = {};
+        var count = 0;
+        var name = "";
+        var temp_uris = {};
+        for (var key in uris) {
+            temp_uris[key] = uris[key];
+            count++;
+            if (count == 1) 
+                name = key.charAt(0) + " - ";
+            else if (count == 10) {
+                name += key.charAt(0);
+                result[name] = temp_uris;
+                temp_uris = {};
+                count = 0;
+            }
+        }
+        return result;
+    }
 };
 
 JOBAD.modules.register(ontologyNavigation);
