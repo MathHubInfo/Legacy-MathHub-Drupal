d4d42a442c38dd8c29bcea905b1d4977ae600e93
aoripov <akbarzoripov@gmail.com>
2015-08-14 21:42:47 +0200

    Change get definition #128

--- sites/all/modules/mmt/jobad/planetary-navigation.js
+++ sites/all/modules/mmt/jobad/planetary-navigation.js
@@ -53,46 +53,149 @@ var planetaryNavigation = {
 			if (uri.match(/\?/g).length >= 2) {	
 				res[showDefS] = function() {
 					$.ajax({ 
-					  'url': mmtUrl + "/:planetary/getRelated",
-	   	  			  'type' : 'POST',
-				      'data' : '{ "subject" : "' + uri + '",' + 
-				      	'"relation" : "isDefinedBy",' + 
-				        '"return" : "planetary"}',
-				       'dataType' : 'html',
-				       'processData' : 'false',
+						'url': mmtUrl + "/:planetary/getRelated",
+	   	  				'type' : 'POST',
+				    	'data' : '{ "subject" : "' + uri + '",' + 
+				    		'"relation" : "isDefinedBy",' + 
+				    		'"return" : "planetary"}',
+				    	'dataType' : 'html',
+				    	'processData' : 'false',
 		       			'contentType' : 'text/plain',
-		              'crossDomain': true,
-	                  'success': function cont(data) {
-	                  	var elem = $("#def_lookup_content");
-	                  	if (elem.length == 0) { //not exists => creating modal 
-		          			$('<div id="def_lookup_main">')
-		          				.addClass('modal fade bs-example-modal-lg')
-		          				.attr({
-		          					'tabindex':'-1', 
-		          					'role':"dialog"
-		          				})
-		      				.append(
-		      					$('<div>')
-		  						.addClass("modal-dialog modal-lg").css('max-width','80%')
-		      					.append(
-		      						$('<div id="def_lookup_content">')
-		      							.addClass("modal-content")
-		      							.html(data)
-		      					)
-		      				).appendTo('body').modal();
-		      			} else { //just updating content
-		      				elem.html(data);
+		            	'crossDomain': true,
+	                	'success': function cont(data) {
+		                  	var elem = $("#def_lookup_content");
+		                  	var result = me.parseServerResponse(data);
+		                  	if (elem.length == 0) { //not exists => creating modal 
+			          			$('<div id="def_lookup_main">')
+			          				.addClass('modal fade')
+			          				.attr({
+			          					'tabindex':'-1', 
+			          					'role':"dialog"
+			          				})
+			      				.append(
+			      					$('<div>')
+			  						.addClass("modal-dialog")
+			      					.append(
+			      						$('<div id="def_lookup_content">')
+			      							.addClass("modal-content")
+			      							.append(
+			      								$('<div>')
+				      								.addClass("modal-header")
+				      								.append(
+				      									$("<button>")
+				      										.addClass("close")
+				      										.attr({
+				      											"type":"button", 
+				      											"data-dismiss":"modal", 
+				      											"aria-label":"Close"})
+				      										.append(
+				      											$("<span>")
+				      												.attr("aria-hidden","true")
+				      												.html("&times;")
+				      										)
+				      								)
+				      								.append(
+		      											$("<h4>")
+		      												.addClass("modal-title")
+		      												.attr("id", "definition-title")
+		      										)
+			      							)
+			      							.append(
+			      								$("<div>")
+			      									.addClass("modal-body")
+			      									.attr("id", "definition-content")
+			      							)
+			      					)
+			      				).appendTo('body');
+			      			}
+		      			 	//updating content
+		      				$("#definition-title").html(result['title']);
+
+		      				if (result['content']) {
+		      					$("#definition-content").html(me.buildTabs(result['content']));
+		      				} else {
+		      					$("#definition-content").html("<div class=\"error\">Not found</div>");
+		      				}
+
 		      				$("#def_lookup_main").modal();
-		      			}
-	                  },
-	                  'error' : function( reqObj, status, error ) {
-						console.log( "ERROR:", error, "\n ",status );
-			    	  },
+		                 },
+	                  	'error' : function( reqObj, status, error ) {
+							console.log( "ERROR:", error, "\n ",status );
+			    	  	},
 	                });
 				};
 			}
 		}
 		return res;
+	},
+	
+	/**
+	* @param response Response from MMT server
+	* @return JS object 
+	*			title The title of definition
+	*			content The content as JS object (to use in buildTabs()) or 
+	*					false if no definition found
+	*/
+	parseServerResponse: function(response) {
+		var subject = $(response).attr("data-subject");
+		var definitions = $(response).find("definition");
+		var result = {};
+		result['title'] = subject;
+		if (definitions.length <= 0) {
+			result['content'] = false;
+		} else {
+			var content = {};
+			jQuery.each(definitions, function(key, definition) {
+				var lang = $(definition).attr("data-lang");
+				content[lang] = $(definition).html();
+			});
+			result['content'] = content;
+		}
+		return result;
+	},
+
+	/**
+	 * @param data Object where key is tab title and value is content of the tab
+	 * @return JS object of the bootstrap implementation of tabs
+	 * first pair is active by default
+	 */
+	buildTabs: function(data) {
+		var out = $("<div>");
+		var navs = $("<ul>").addClass("nav nav-tabs").attr("role", "tablist");
+		var tabs = $("<div>").addClass("tab-content");
+		var active = true;
+		$.each(data, function(title, content) {
+			var nav = $("<li>")
+				.attr("role", "presentation")
+				.append(
+					$("<a>")
+						.attr({
+							"href":"#" + title,
+							"aria-controls":title,
+							"role":"tab",
+							"data-toggle":"tab",
+						})
+						.html(title)
+				);
+			var tab = $("<div>")
+				.addClass("tab-pane")
+				.attr({
+					role:"tabpanel",
+					id:title
+				})
+				.html(content);
+			
+			if (active) {
+				nav.addClass("active");
+				tab.addClass("active");
+				active = false;
+			}
+
+			navs.append(nav);
+			tabs.append(tab);
+		});
+		out.append(navs).append(tabs);
+		return out;
 	},
     
 };
